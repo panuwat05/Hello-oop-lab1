028f905603a674899271719c7b70afe5d8f1026d
panuwat.prasertdee <panuwat.pra@kkumail.com>
2024-11-26 11:13:21 +0700

    add C#

---
+++ Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Panuwat Prasertdee";
+string student_id = "673450474-0";
+// string is Datatype -> ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Panuwat Prasertdee");
+Console.WriteLine("My student id : 673450474-0");
+
